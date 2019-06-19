1f1229285be7ff4b4629d40b86cfc1345a00062a
Varun Rathi <varun.rathi@jeevansathi.com>
2019-06-19 23:03:27 +0530

    1. Move validation logic for Pawn:- One step, Two step, Diagonal

--- Chess/Pieces/Pawn.swift
+++ Chess/Pieces/Pawn.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class Pawn: UIChessPiece {
     
-  init(frame:CGRect, color:UIColor, vc:ChessViewController,pieceColor:PieceColor = .White)  {
+    var tryAdvanceTwoSteps:Bool = false
+    
+    init(frame:CGRect, color:UIColor, vc:ChessViewController,pieceColor:PieceColor = .White)  {
         super.init(frame: frame)
         
         if (pieceColor ==  PieceColor.Black){
-                self.text = "♙"
+            self.text = "♙"
         }else
         {
-                self.text = "♙"
+            self.text = "♙"
         }
-    
+        
         self.symbol = pieceColor
         self.isOpaque = false
         self.textColor = color
@@ -35,8 +37,34 @@ class Pawn: UIChessPiece {
         super.init(coder: aDecoder)!
     }
     
-    override func isMovePossible(from source: BoardIndex, destinationIndex: BoardIndex) -> Bool {
-    return true
-}
+    override func isMovePossible(from source: BoardIndex, destination: BoardIndex) -> Bool {
+        
+        // Two steps check
+        if source.col == destination.col {
+            
+            if symbol == .White && source.row == 6 && destination.row == 4 {
+                tryAdvanceTwoSteps = true
+                return true
+            }
+            else if symbol == .Black && source.row == 1 && destination.row == 3{
+                tryAdvanceTwoSteps = true
+                return true
+            }
+            
+        }
+        
+        tryAdvanceTwoSteps = false
+        
+        let moveForward = symbol == .Black ? 1 : -1
+        
+        if destination.row == source.row  + moveForward {
+            
+            if destination.col == source.col - 1 || destination.col == source.col || destination.col == source.col + 1 {
+                return true
+            }
+        }
+        
+        return true
+    }
     
 }
